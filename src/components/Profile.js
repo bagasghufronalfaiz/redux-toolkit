c018623a64e5de55fba4e68c09c43768968ef536
Bagas Ghufron Alfaiz <bagasghufronalfaiz@gmail.com>
2022-02-08 13:21:27 +0700

    profile

---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Profile() {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <div>
+                <p>Name : </p>
+                <p>Email : </p>
+                <p>Age : </p>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
